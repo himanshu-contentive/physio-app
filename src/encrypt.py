01a8f363abd49b8e2a452eac1219b2b770648bd3
himanshu-contentive <himanshu.kumar@contentive.com>
2023-09-13 13:16:38 +0530

    Added a file to demonstrate encryption in flask

---
+++ src/encrypt.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+
+# Generate a key for encryption and decryption
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+# Encrypt the text
+def encrypt_text(text):
+    encrypted_text = cipher_suite.encrypt(text.encode())
+    return encrypted_text
+
+# Decrypt the text
+def decrypt_text(encrypted_text):
+    decrypted_text = cipher_suite.decrypt(encrypted_text).decode()
+    return decrypted_text
+
+@app.route('/')
+def index():
+    # Your text to encrypt
+    plaintext = "OPEN_AI_KEY = 'Himanshu Kumar'"
+    
+    # Encrypt the text
+    encrypted_data = encrypt_text(plaintext)
+
+    # Decrypt the text (for demonstration purposes)
+    decrypted_data = decrypt_text(encrypted_data)
+
+    return f"Original Text: {plaintext}<br>Encrypted Text: {encrypted_data}<br>Decrypted Text: {decrypted_data}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
